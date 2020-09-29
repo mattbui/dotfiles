c006cf09c29497854f794397312becb15a4fc947
MatthewBui <matthew@cinnamon.is>
2020-09-29 11:15:05 +0700

    Add more stuff to initialize scripts: * Download powerline10k instead of powerline9k * Git config global rebase instead of merge on pull * Add .gitignore_global file

--- initialize.sh
+++ initialize.sh
@@ -1,17 +1,27 @@
-# Initialize commands for new servers
+# Initialize commands for new computer
 
 # Zshell
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 cp .zshrc ~/
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "exec zsh" >> ~/.bashrc
+cp .p10k.zsh ~/
+
+# use this if cannot change default shell to zsh
+# echo "exec zsh" >> ~/.bashrc
 
 # Git
-mkdir ~/.ssh
-cp cinnamon_git.key ~/.ssh
-gpg --import gpg_secret_keys
+# mkdir ~/.ssh
+# add git key if needed
+# cp git.key ~/.ssh
+# gpg --import gpg_secret_keys
 git config --global user.email matthew@cinnamon.is
-git config --global user.name matt_Cin
-git config --global user.signingkey 24AE1F8C294EE08C
+git config --global user.name "Bui Cong Minh"
+git config --global user.username mattbui
+git config --global branch.autosetuprebase always
+
+cp .gitignore_global ~/
+git config --global core.excludesfile ~/.gitignore_global
+
+# git config --global user.signingkey 24AE1F8C294EE08C
 git config --global commit.gpgsign true
