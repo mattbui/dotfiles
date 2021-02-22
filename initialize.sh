7b4349d95e071ba7f6978e9dd36fb47302e1bf25
MatthewBui <matthew@cinnamon.is>
2021-02-22 17:06:16 +0700

    update todo

--- initialize.sh
+++ initialize.sh
@@ -6,7 +6,7 @@ dotfiles=$HOME/dotfiles
 git clone https://github.com/mattbui/dotfiles.git $dotfiles
 
 # Zshell
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"  # TODO: install oh-my-zsh in silent mode
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
