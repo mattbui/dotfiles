6a48a7b88bf540832b54c0cb854e4e34caddcef6
MatthewBui <matthew@cinnamon.is>
2021-02-25 15:22:43 +0700

    alias: update

--- dotzsh/.aliases.zsh
+++ dotzsh/.aliases.zsh
@@ -1,10 +1,12 @@
 # My aliases
-alias e='conda activate'
+alias ea='conda activate'
 alias ed='conda deactivate'
 alias ei='conda install'
 alias ne='conda create -n'
 alias le='conda env list'
 alias re='conda remove --all -n'
+
+alias e=$EDITOR
 
 alias af='alias-finder'
 alias cheat='cht.sh'
