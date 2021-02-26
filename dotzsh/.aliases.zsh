3ff1c78d569f1858c520a09af5c3c3657eb7148d
MatthewBui <matthew@cinnamon.is>
2021-02-26 17:35:14 +0700

    zsh: change conda aliases

--- dotzsh/.aliases.zsh
+++ dotzsh/.aliases.zsh
@@ -1,12 +1,12 @@
 # My aliases
-alias ea='conda activate'
-alias ed='conda deactivate'
-alias ei='conda install'
-alias ne='conda create -n'
-alias le='conda env list'
-alias re='conda remove --all -n'
+alias ca='conda activate'
+alias cde='conda deactivate'
+alias ci='conda install'
+alias cc='conda create -n'
+alias cl='conda env list'
+alias crm='conda remove --all -n'
 
-alias e=$EDITOR
+alias e=$VISUAL
 
 alias af='alias-finder'
 alias cheat='cht.sh'
