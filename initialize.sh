2881627449f952c063f5b263f39c95e891a2e37b
MatthewBui <matthew@cinnamon.is>
2021-03-02 10:52:11 +0700

    nvim: remove conda setup script for now

--- initialize.sh
+++ initialize.sh
@@ -4,15 +4,19 @@
 DOTFILES=$HOME/dotfiles
 git clone https://github.com/mattbui/dotfiles.git $DOTFILES
 
-# Zshell
-# TODO: source init.zsh need to be added at the begining of .zshrc file
-echo "source $HOME/.config/zsh/init.zsh" >> $HOME/.zshrc
-curl -L git.io/antigen > $HOME/.antigen.zsh  # antigen as zsh plugins manager
 sh $DOTFILES/init/link.sh  # link configs from dotfiles
+
+# Zshell
+# Add "source $HOME/.config/zsh/init.zsh" at the begining of $HOME/.zshrc
+(echo "source $HOME/.config/zsh/init.zsh" &&\
+    cat $HOME/.zshrc 2>/dev/null) > $HOME/.zshrc_tmp\
+    && mv $HOME/.zshrc_tmp $HOME/.zshrc
+
+curl -L git.io/antigen > $HOME/.antigen.zsh  # antigen as zsh plugins manager
 
 
 sh $DOTFILES/init/git.sh  # git configs
-sh $DOTFILES/init/conda.sh  # setup miniconda
+# sh $DOTFILES/init/conda.sh  # setup miniconda
 
 # use this if cannot change default shell to zsh
 printf "Done intialization"
