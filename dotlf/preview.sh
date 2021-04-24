83e406855d42b7b4c2a5b5e03363288c2e6aa1aa
MatthewBui <matthew@cinnamon.is>
2021-04-24 22:49:52 +0700

    lf: update

--- dotlf/preview.sh
+++ dotlf/preview.sh
@@ -5,6 +5,5 @@ case "$1" in
     *.zip) unzip -l "$1";;
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
-    *.pdf) pdftotext "$1" -;;
     *) bat --color=always --style=numbers --line-range :100 "$1";;
 esac
