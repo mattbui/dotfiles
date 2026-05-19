e971bfe0c28fa9850149d0fe8c3e5544b5365f5a
Minh Bùi <buicongminh2717@gmail.com>
2026-05-19 16:50:19 +0700

    chore(tmux): show ssh target in status
    - add an ssh wrapper that resolves the target from ssh -G inside tmux
    - set the pane title to the resolved host or user@hostname while ssh is active
    - keep the tmux command segment as ssh and show the target in the path segment

--- dotzsh/aliases.zsh
+++ dotzsh/aliases.zsh
@@ -24,3 +24,40 @@ alias ppip='python -m pip'
 alias pipython='python -c "import IPython; IPython.terminal.ipapp.launch_new_instance()"'
 
 [ -z $(command -v brew) ] || alias ctags="`brew --prefix`/bin/ctags"
+
+ssh() {
+    local title
+    local status
+
+    if [[ -n "$TMUX" ]]; then
+        title=$(command ssh -G "$@" 2>/dev/null | awk '
+            $1 == "host" { host = $2 }
+            $1 == "user" { user = $2 }
+            $1 == "hostname" { hostname = $2 }
+            END {
+                if (host != "") {
+                    printf "%s", host
+                    exit
+                }
+
+                if (hostname == "") {
+                    exit 1
+                }
+
+                if (user != "") {
+                    printf "%s@%s", user, hostname
+                } else {
+                    printf "%s", hostname
+                }
+            }
+        ')
+
+        [[ -n "$title" ]] && tmux select-pane -T "$title"
+    fi
+
+    command ssh "$@"
+    status=$?
+
+    [[ -n "$TMUX" && -n "$title" ]] && tmux select-pane -T ""
+    return $status
+}
