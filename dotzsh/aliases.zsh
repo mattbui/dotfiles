1b6ecb7736a6f7438346fcbee74ad3891535762d
Minh Bùi <buicongminh2717@gmail.com>
2026-05-21 05:38:18 +0700

    chore(zsh): remove conda aliases

--- dotzsh/aliases.zsh
+++ dotzsh/aliases.zsh
@@ -1,14 +1,5 @@
 # My aliases
 
-# conda
-alias ca='conda activate'
-alias cde='conda deactivate'
-alias ci='conda install'
-alias cc='conda create -n'
-alias cl='conda env list'
-alias cr='conda remove'
-alias cra='conda remove --all -n'
-
 # workaround when using e command inside floaterm/vim
 [ -z $VIMRUNTIME ] && alias e=$EDITOR || alias e=floaterm
 alias v=$VISUAL
