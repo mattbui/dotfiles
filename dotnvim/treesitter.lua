86333e83fcd55198f7e62bf89ffe319041966f90
Minh Bùi <buicongminh2717@gmail.com>
2026-05-21 05:37:17 +0700

    fix(nvim): skip problematic treesitter parsers

--- dotnvim/treesitter.lua
+++ dotnvim/treesitter.lua
@@ -1,8 +1,8 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ignore_install = {}, -- List of parsers to ignore installing
+  ignore_install = {"ipkg"}, -- List of parsers to ignore installing
   highlight = {
     enable = true,              -- false will disable the whole extension
-    disable = {},  -- list of language that will be disabled
+    disable = {"dockerfile"},  -- list of language that will be disabled
   },
 }
