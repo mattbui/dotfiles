75d575685df45f062d07a92fdb1e7c9a4881b646
Minh Bùi <buicongminh2717@gmail.com>
2026-07-12 22:32:04 +0700

    refactor(nvim/neoscroll): simplify scroll easing

--- dotnvim/lua/plugins/neoscroll.lua
+++ dotnvim/lua/plugins/neoscroll.lua
@@ -1,11 +1,8 @@
 require("neoscroll").setup({
-  mappings = { "<C-u>", "<C-d>", "zt", "zz", "zb" },
+  mappings = { "<C-u>", "<C-d>" },
   hide_cursor = true,
   stop_eof = true,
   respect_scrolloff = false,
   cursor_scrolls_alone = true,
-  easing = false,
-  easing_function = function(x)
-    return x ^ 4
-  end,
+  easing = "linear",
 })
