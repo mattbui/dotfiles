a0e1a07c8889ffc5f5ccf4841912be0550ddfec7
Minh Bùi <buicongminh2717@gmail.com>
2026-07-17 07:03:18 +0700

    fix(tmux/launcher): align enter key hint

--- dottmux/scripts/tmux-launcher.sh
+++ dottmux/scripts/tmux-launcher.sh
@@ -168,7 +168,7 @@ result="$({
       --height=100% \
       --reverse \
       --border 'sharp' \
-      --border-label ' tmux launcher · ↵: new window · ^n: new session · ^a: all · ^f: dirs · ^s: ssh ' \
+      --border-label ' tmux launcher · ↵ : new window · ^n: new session · ^a: all · ^f: dirs · ^s: ssh ' \
       --prompt '📺 ' \
       --bind 'ctrl-a:change-prompt(📺 )+reload("$HOME/.config/tmux/scripts/tmux-launcher.sh" --list-all)' \
       --bind 'ctrl-f:change-prompt(🔎 )+reload("$HOME/.config/tmux/scripts/tmux-launcher.sh" --list-directories)' \
